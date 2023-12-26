9a210ed121a1ac39339240c69dfa341b2d70e8f7
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-12-26 12:19:58 +0100

    docker set up

--- src/main/java/com/mytaskapp/events/OrganizerRepository.java
+++ src/main/java/com/mytaskapp/events/OrganizerRepository.java
@@ -14,6 +14,7 @@ public class OrganizerRepository {
         return organizers;
     }
 }
+//// http://localhost:8080/organizers
 
 // For other repos
 // https://app.pluralsight.com/ilx/video-courses/a9b638e4-bb07-4cee-82f4-a2b93d5b8866/3e9835d1-6e6d-4384-891f-b513a7d19147/2166387c-7247-464f-b31e-edcdd0c3f06b
